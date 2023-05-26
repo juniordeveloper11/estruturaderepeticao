5ebf34f6d4ad1365e526a32fcb1302568f720580
juniordeveloper11 <89851068+juniordeveloper11@users.noreply.github.com>
2023-05-25 22:30:25 -0300

    Add files via upload

---
+++ estruturaDeRepicaoWhile.java
@@ -0,0 +1,24 @@
+package estruturaDeRepeticao;
+
+import java.util.Iterator;
+
+public class estruturaDeRepicaoWhile {
+	
+	public static void main(String[] args) {
+		
+		int count = 0;
+		while (count< 20) {
+			System.out.println(++count);
+			
+		}
+			
+				for (int i = 0; i < 10 ; i++) {
+					System.out.println("for"+i);
+					
+				}
+			}
+		}
+		
+	
+
+
